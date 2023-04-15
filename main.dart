2423629b8b58880d1b43219ee937e1fa563683d1
GittBittu <Budhaditya.Nayak@outlook.com>
2023-04-15 19:42:00 +0530

    some modifications to first code

--- main.dart
+++ main.dart
@@ -1,4 +1,16 @@
 void main() {
   print("hello world");
   print("hello dart");
+  int a;
+  a=7;
+  print(a);
+  a=8;
+  print(a);
+  String name="Budhaditya";
+  BigInt longvalue=BigInt.parse('21423412532543215145128747283572362');
+  print("This is a longvalue $longvalue");
+  double percent=63.45;
+  num one=12;
+  num two=12.12;
+  bool login=false;
 }
